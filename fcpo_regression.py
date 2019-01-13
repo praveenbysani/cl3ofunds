deb6f638d96323bb707a871bba85f2f57bd2ab3a
Praveen <praveen.iiith@gmail.com>
2019-01-13 23:17:06 +0800

    new lstm model for multistep regression added

--- fcpo_regression.py
+++ fcpo_regression.py
@@ -143,11 +143,11 @@ def build_regression_model_data(fcpo_data_daily,fcpo_feats,target_label='long_sh
     fcpo_train=fcpo_feats[train_start:train_end]
     fcpo_test = fcpo_feats[test_start:test_end]
 
-    fcpo_train=fcpo_train.merge(fcpo_data_daily[['Open','prev_open_change_pct',target_label]],left_index=True,right_index=True)
+    fcpo_train=fcpo_train.merge(fcpo_data_daily[['Open','prev_1day_ret','prev_open_change_pct',target_label]],left_index=True,right_index=True)
     fcpo_traindata=fcpo_train.iloc[:,0:-1]
     fcpo_trainlabels=fcpo_train.iloc[:,-1:]
 
-    fcpo_test=fcpo_test.merge(fcpo_data_daily[['Open','prev_open_change_pct',target_label]],left_index=True,right_index=True)
+    fcpo_test=fcpo_test.merge(fcpo_data_daily[['Open','prev_1day_ret','prev_open_change_pct',target_label]],left_index=True,right_index=True)
     fcpo_testdata=fcpo_test.iloc[:,0:-1]
     fcpo_testlabels=fcpo_test.iloc[:,-1]
     return fcpo_traindata,fcpo_trainlabels,fcpo_testdata,fcpo_testlabels
@@ -165,14 +165,18 @@ if __name__=='__main__':
     #TODO : drop highly correlated features
 
     fcpo_traindata,fcpo_trainlabels,fcpo_testdata,fcpo_testlabels=build_regression_model_data(
-                            fcpo_data_daily,fcpo_daily_tind,'long_spread')
+                            fcpo_data_daily,fcpo_feats,'long_short_spread_diff')
 
     from sklearn.preprocessing import RobustScaler, MinMaxScaler
-    robust_scaler=RobustScaler()
-    fcpo_scaled_trainlabels=robust_scaler.fit_transform(fcpo_trainlabels)
+    robust_scaler_labels=RobustScaler()
+    robust_scaler_data=RobustScaler()
+
+    fcpo_scaled_trainlabels=robust_scaler_labels.fit_transform(fcpo_trainlabels)
+    fcpo_scaled_traindata=robust_scaler_data.fit_transform(fcpo_traindata)
+
     fcpo_scaled_testlabels=pd.Series(robust_scaler.transform(np.array(fcpo_testlabels).reshape(1,-1))[0],
                                             index=fcpo_testlabels.index)
-
+    fcpo_scaled_testdata=robust_scaler_data.transform(fcpo_testdata)
    '''
     xgb_param_dist={
                "n_estimators": [200,300,500],
@@ -185,22 +189,24 @@ if __name__=='__main__':
                            xgb_param_dist,random_state=333,cv=fcpo_tsplit,scoring='neg_mean_absolute_error',n_iter=100,n_jobs=-1)
     xgb_rsearch.fit(fcpo_traindata,np.ravel(fcpo_trainlabels))
     mean_absolute_error(fcpo_testlabels,xgb_regressor.predict(fcpo_testdata))
-    '''
-
-    xgb_regressor=xgboost.XGBRegressor(n_estimators=200,max_depth=5,learning_rate=0.01,gamma=0.05)
 
     fcpo_tsplit=TimeSeriesSplit(n_splits=3)
     fcpo_cval_scores=cross_val_score(xgb_regressor,fcpo_traindata,np.ravel(fcpo_trainlabels),
                                        scoring='neg_mean_squared_error',cv=fcpo_tsplit)
 
-    xgb_regressor=xgb_regressor.fit(fcpo_traindata,np.ravel(fcpo_trainlabels))
+    '''
+
+    xgb_regressor=xgboost.XGBRegressor(n_estimators=200,max_depth=5,learning_rate=0.01,gamma=0.05)
+
+
+    xgb_regressor=xgb_regressor.fit(fcpo_traindata,fcpo_trainlabels)
     fcpo_pred_values=xgb_regressor.predict(fcpo_testdata)
-    fcpo_pred_rescaled_values=robust_scaler.inverse_transform(np.array(fcpo_pred_values).reshape(1,-1))
+
+    fcpo_pred_rescaled_values=robust_scaler_labels.inverse_transform(np.array(fcpo_pred_values).reshape(1,-1))
     fcpo_pred_rescaled_values=pd.Series(fcpo_pred_rescaled_values[0],index=fcpo_testlabels.index)
     fcpo_pred_values=pd.Series(fcpo_pred_values,index=fcpo_testlabels.index)
-    mean_absolute_error(fcpo_scaled_testlabels,fcpo_pred_values)
-
+    mean_absolute_error(fcpo_testlabels,fcpo_pred_values)
 
     plt.figure(figsize=(10,10))
-    fcpo_scaled_testlabels.plot()
-    fcpo_pred_values.plot()
+    fcpo_testlabels.plot()
+    pd.Series(fcpo_pred_values,index=fcpo_testlabels.index).plot()
